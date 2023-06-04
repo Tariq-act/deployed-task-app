61627021b3d035a5fc178ef82e808fc586bcbd9e
Tariq-act <khantarikact@gmail.com>
2023-06-05 00:56:40 +0530

    add deployed postgres database

--- server/db.js
+++ server/db.js
@@ -6,7 +6,32 @@ const pool = new Pool({
   password: process.env.PASSWORD,
   host: process.env.HOST,
   port: process.env.DBPORT,
-  database: 'todoapp',
+  database: process.env.DATABASE,
+  connectionTimeoutMillis: 5000, // Increase timeout value in milliseconds
+  idleTimeoutMillis: 30000, // Increase idle timeout value in milliseconds
+  ssl: true,
 });
+
+// Attempt to connect and execute a test query
+// pool.connect((error, client, release) => {
+//   if (error) {
+//     console.error('Error connecting to the database:', error);
+//     return;
+//   }
+
+//   client.query('SELECT 1', (queryError, result) => {
+//     release(); // Release the client back to the pool
+
+//     if (queryError) {
+//       console.error('Error executing test query:', queryError);
+//       return;
+//     }
+
+//     console.log('Successfully connected to the database');
+//     // You can perform additional operations or start your application logic here
+
+//     pool.end(); // Close the connection pool when you're done
+//   });
+// });
 
 module.exports = pool;
